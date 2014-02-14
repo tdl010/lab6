4cdbfec4ca7ee19f213f9bdac079bba6fab80771
vagrant <vagrant@precise32.(none)>
2014-02-14 21:11:36 +0000

    ajax lab

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
-
+var palette = require('./routes/palette')
 var index = require('./routes/index');
 var project = require('./routes/project');
 // Example route
@@ -38,6 +38,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.projectInfo);
+app.get('/palette', palette.randomPalette);
 // Example route
 // app.get('/users', user.list);
 
--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,13 +26,39 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProjectDetails1); //addProjectDetails calls function
+
+
 	console.log("User clicked on project " + idNumber);
 }
+
+function addProjectDetails1(result){
+	console.log(result);
+	var projectHTML = 
+		'<img src ="' + result['image'] + '"class = "detailsImage">' +
+		'<p>' + result['date'] + '</p>' + 
+		'<p><small>' + result['summary'] + '</small></p>';
+	$("#project" + result['id'] + " .details").html(projectHTML);
+}
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
+
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", changeColors);
+}
+
+function changeColors(result){
+	console.log('changingcolors...');
+	var colors = result['colors']['hex'];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
